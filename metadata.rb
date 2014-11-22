a635ebbcede76a77b1ec858033bd8c895cbf9155
Diego Lapiduz <diego.lapiduz@gsa.gov>
2014-11-21 17:12:37 -0800

    Update shipper

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer       "Diego Lapiduz"
 maintainer_email "diego.lapiduz@gsa.gov"
 description      "Setup shipper to allow continuous deployments"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.1"
+version          "0.1.2"
--- resources/install.rb
+++ resources/install.rb
@@ -3,7 +3,7 @@ actions :install
 attribute :path, :kind_of => String, :default => '/etc/shipper'
 attribute :package_url,
           :kind_of => String,
-          :default => 'https://github.com/18f/shipper/releases/download/v0.1.1/shipper'
+          :default => 'https://github.com/18f/shipper/releases/download/v0.1.2/shipper'
 
 
 
